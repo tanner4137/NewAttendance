5ef962d576ea4c926d2739dac5b7873a18f7339d
Tanner <tanner4137>
2017-12-12 18:57:48 -0600

    Commit Two (YAY)

--- AttendanceCheckerSystem/Startup.cs
+++ AttendanceCheckerSystem/Startup.cs
@@ -21,6 +21,7 @@ namespace AttendanceCheckerSystem
             Configuration = configuration;
         }
         //
+        //
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
